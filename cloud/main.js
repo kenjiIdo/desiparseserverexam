dabd3862a07b48d1eb4fd940cc7726d7f725ce82
KenjiIdo <kenji0405@hotmail.com>
2016-03-11 17:48:01 +0900

    no message

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,9 @@
-require('cloud/installation.js');
-require('cloud/activity.js');
-require('cloud/photo.js');
-require('cloud/masterKeyFunctions.js');
-require('cloud/user.js');
-require('cloud/backgroundJob.js');
+Parse.Cloud.define('hello', function(req, res) {
+  res.success('Hi');
+});
+//require('cloud/installation.js');
+//require('cloud/activity.js');
+//require('cloud/photo.js');
+//require('cloud/masterKeyFunctions.js');
+//require('cloud/user.js');
+//require('cloud/backgroundJob.js');
