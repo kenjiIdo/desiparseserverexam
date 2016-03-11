bb965e7766547549fefcf90eac87cbeb43e42f94
KenjiIdo <kenji0405@hotmail.com>
2016-03-11 17:44:58 +0900

    add cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,6 @@
-
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
-});
+require('cloud/installation.js');
+require('cloud/activity.js');
+require('cloud/photo.js');
+require('cloud/masterKeyFunctions.js');
+require('cloud/user.js');
+require('cloud/backgroundJob.js');
